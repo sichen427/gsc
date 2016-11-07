1cab38a0e0e9594ad93734a0c725d710c7a633c5
katsucherry <742613496@qq.com>
2016-11-07 16:39:15 +0800

    init

---
+++ shengci/WebContent/shengci/gscShengciController.js
@@ -0,0 +1,56 @@
+﻿angular.module('shengci', []);
+
+//定义生成随机数
+var rand = {};
+rand.get = function (begin,end){
+ return Math.floor(Math.random()*(end-begin))+begin;
+};
+
+//var v = rand.get(1,10)
+
+function gscShengciController($scope, $http) {
+	//范围数据
+  //$scope.danci = ["a","b","c","d","e"];
+  //$http.get('../ShengciServlet').success(function(response) {
+  $http.get('http://localhost:8080/shengci_server/ShengciServlet').success(function(response) {
+	  $scope.danci =  response.data;
+	  
+	//范围数据对应的seq
+	  //$scope.seq = [0,1,2,3,4];
+	  $scope.seq = [];
+	  //当前数据(一个)
+	  $scope.currentDanci = 0;
+		
+		var loopCount = 0;
+		while ($scope.seq.length < $scope.danci.length ) {
+			loopCount += 1;
+			//取随机数
+			var randomSeq = rand.get(0,$scope.danci.length);
+			//现有的seq数组中不存在的情况下才追加
+			if ($scope.seq.indexOf(randomSeq) == -1) {
+				$scope.seq.push(randomSeq);
+			}
+
+			if (loopCount > 5000) {
+				console.log("dead loop...");
+				break;
+			}
+		}
+	  
+  });
+
+  	//后一个
+  	$scope.nextDanci = function() {
+  		if (($scope.currentDanci + 1) < $scope.danci.length) {
+  			$scope.currentDanci += 1;
+  		}
+  	}
+
+  	//前一个
+  	$scope.preDanci = function() {
+  		if (($scope.currentDanci + 1) > 1) {
+  			$scope.currentDanci -= 1;
+  		}
+  	}
+  	
+}
---
+++ shengci/src/cn/gsc/shengci/ShengciServlet.java
@@ -0,0 +1,67 @@
+package cn.gsc.shengci;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Servlet implementation class ShengciServlet
+ */
+public class ShengciServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShengciServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		List<String> list = new ArrayList<String>();
+		list.add("aa");
+		list.add("bb");
+		list.add("cc");
+		list.add("dd");
+		list.add("ee");
+		list.add("ff");
+		list.add("gg");
+		list.add("hh");
+		list.add("ii");
+		list.add("jj");
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("data", list);
+        String json = jsonObject.toJSONString();
+        
+        //out.write(json.toString());
+        out.println(json);
+        out.flush();
+        out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
